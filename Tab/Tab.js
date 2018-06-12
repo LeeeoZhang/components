5e0a649ee1d6b1a1d4d14643627c7af1aa857279
leo <leozhang621@gmail.com>
2018-06-12 14:27:22 +0800

    rebuild Tab

--- Tab/Tab.js
+++ Tab/Tab.js
@@ -1,59 +1,52 @@
-//封装
-let Tab = (function() {
-    function bind(tabContainer, contentContainer) {
-        let tabItems = tabContainer.querySelectorAll('li') //bind()从外部无法被访问
-        let contentItems = contentContainer.querySelectorAll('li')
-        tabContainer.addEventListener('click', function(e) {
-            let clickTarget = e.target
-            if (clickTarget.tagName.toLowerCase() === 'li') {
-                tabItems.forEach(function(item, index) {
-                    item.classList.remove('active')
-                })
-                clickTarget.classList.add('active')
-                let targetIndex = [].indexOf.call(tabItems, clickTarget)
-                contentItems.forEach(function(item, index) {
-                    item.classList.remove('show')
-                })
-                contentItems[targetIndex].classList.add('show')
-            }
-        })
+/*
+* Tab组件
+* @param {Node} tabCt tab container
+* @param {Node} contentCt 内容container
+* */
+
+!function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory)
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory()
+    } else {
+        root.Tab = factory()
     }
-    return {
-        init: function(tabContainer, contentContainer) {
-            bind(tabContainer, contentContainer)
+}(window, () => {
+    class Tab {
+        constructor (tabCt, contentCt) {
+            this.tabCt = tabCt
+            this.contentCt = contentCt
+            this.init()
+        }
+
+        init () {
+            this.bind()
+        }
+
+        bind () {
+            const tabCt = this.tabCt
+            const contentCt = this.contentCt
+            const tabItems = tabCt.querySelectorAll('li')
+            const contentItems = contentCt.querySelectorAll('li')
+            tabCt.addEventListener('click', (event) => {
+                let target = event.target
+                if (target.tagName.toLowerCase() === 'li') {
+                    tabItems.forEach(function (item, index) {
+                        item.classList.remove('active')
+                    })
+                    target.classList.add('active')
+                    let targetIndex = [].indexOf.call(tabItems, target)
+                    contentItems.forEach(function (item, index) {
+                        item.classList.remove('show')
+                    })
+                    contentItems[targetIndex].classList.add('show')
+                }
+            })
         }
     }
-})()
-Tab.init(document.querySelectorAll('.tab-container>.tab')[0], document.querySelectorAll('.tab-container>.content')[0])
-Tab.init(document.querySelectorAll('.tab-container>.tab')[1], document.querySelectorAll('.tab-container>.content')[1])
+
+    return Tab
+})
 
 
-
-//面对对象写法
-// function Tab(tabContainer,contentContainer) {
-//     this.tabContainer = tabContainer
-//     this.contentContainer = contentContainer
-//     this.tabItems = this.tabContainer.querySelectorAll('li')
-//     this.contentItems = this.contentContainer.querySelectorAll('li')
-//     this.bind()
-// }
-// Tab.prototype.bind = function() {
-//     let _this = this
-//     this.tabContainer.addEventListener('click',function(e) {
-//         let clickTarget = e.target
-//         if(clickTarget.tagName.toLowerCase() === 'li') {
-//             _this.tabItems.forEach(function(item,index) {
-//                 item.classList.remove('active')
-//             })
-//             clickTarget.classList.add('active')
-//             let targetIndex = [].indexOf.call(_this.tabItems,clickTarget)
-//             _this.contentItems.forEach(function(item,index) {
-//                 item.classList.remove('show')
-//             })
-//             _this.contentItems[targetIndex].classList.add('show')
-//         }
-//     })
-// }
-//
-// new Tab(document.querySelectorAll('.tab-container>.tab')[0],document.querySelectorAll('.tab-container>.content')[0])
-// new Tab(document.querySelectorAll('.tab-container>.tab')[1],document.querySelectorAll('.tab-container>.content')[1])
