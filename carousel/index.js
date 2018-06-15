97b2156544ac2b6d5583cbfc194637a9bdff8ade
leo <leozhang621@gmail.com>
2018-06-15 10:37:12 +0800

    update carousel

--- carousel/index.js
+++ carousel/index.js
@@ -7,12 +7,16 @@
             this.wrap2 = option.wrap2
             this.pageNum = option.pageNum
             this.eachWidth = option.eachWidth
-            this.index = 1
+            this.onTransitionEnd = option.onTransitionEnd
+            this.preIndex = -1
+            this.currentIndex = 0
+            this.nextIndex = 1
             this.lock = false
             this.bind()
         }
 
         bind () {
+            this.wrap2.style.width = this.eachWidth * this.pageNum
             this.wrap1.addEventListener('touchstart', (event) => {
                 this.startX = event.touches[0].pageX
             })
@@ -28,19 +32,32 @@
                     console.log('左滑')
                     this.next()
                 }
+                this.diffX = this.startX = 0
+            })
+            this.wrap2.addEventListener('webkitTransitionEnd',(event)=>{
+                this.onTransitionEnd && this.onTransitionEnd(this.currentIndex)
             })
         }
 
         next () {
-            if (this.index >= this.pageNum - 1) this.index = this.pageNum - 1
-            this.wrap2.style.transform = `translateX(${-this.eachWidth * this.index}px)`
-            this.index += 1
+            if (this.nextIndex > this.pageNum - 1){
+                this.nextIndex = this.pageNum - 1
+            } else {
+                this.preIndex += 1
+                this.currentIndex += 1
+            }
+            this.wrap2.style.transform = `translateX(${-this.eachWidth * this.nextIndex}px)`
+            this.nextIndex += 1
         }
 
         pre () {
-            this.index -= 1
-            if (this.index < 0) this.index = 0
-            this.wrap2.style.transform = `translateX(${-this.eachWidth * this.index}px)`
+            if (this.preIndex < 0) {
+                this.preIndex = 0
+            } else {
+                this.nextIndex -= 1
+                this.currentIndex -= 1
+            }
+            this.wrap2.style.transform = `translateX(${-this.eachWidth * this.preIndex}px)`
         }
     }
 
